62435ab19a81b809d208faeb6c0df1bfc3a7d71c
Richard Warren <rwarren92@hotmail.com>
2016-07-12 15:34:08 +0100

    start

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const handler = require('./handler.js')
+const server = http.createServer(handler);
+
+const port = 4000;
+
+function startServer() {
+  server.listen(process.env.PORT || port);
+  console.log('.txt file has loaded and Server is running on: ' + port);
+};
+
+startServer();
+
+module.exports = {
+    startServer: startServer
+}
