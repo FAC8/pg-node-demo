f9ae0126207f5190244b841cf022632b57dd7ded
Matt Glover <matthewglover@me.com>
2016-07-12 16:23:24 +0100

    remove redis

--- src/handler.js
+++ src/handler.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
 const pg = require('pg');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, (err, client) => {
-  if (err) throw err;
+// pg.defaults.ssl = true;
+// pg.connect(process.env.DATABASE_URL, (err, client) => {
+//   if (err) throw err;
+//
+//   console.log('connected to postgres! Getting schemas...');
+//
+//   client
+//     .query('SELECT table_schema, table_name FROM information_schema.tables;')
+//     .on('row', (row) => {
+//       console.log(JSON.stringify(row));
+//     });
+// });
 
-  console.log('connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema, table_name FROM information_schema.tables;')
-    .on('row', (row) => {
-      console.log(JSON.stringify(row));
-    });
-});
-
-var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+// var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
 
 const handler = (req, res) => {
