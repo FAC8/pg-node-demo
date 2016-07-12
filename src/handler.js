7d6da7e094ce8ef4523160ec4ff4c57566f6c164
Matt Glover <matthewglover@me.com>
2016-07-12 16:29:28 +0100

    add database connection test

--- src/handler.js
+++ src/handler.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const pg = require('pg');
 
-// pg.defaults.ssl = true;
-// pg.connect(process.env.DATABASE_URL, (err, client) => {
-//   if (err) throw err;
-//
-//   console.log('connected to postgres! Getting schemas...');
-//
-//   client
-//     .query('SELECT table_schema, table_name FROM information_schema.tables;')
-//     .on('row', (row) => {
-//       console.log(JSON.stringify(row));
-//     });
-// });
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, (err, client) => {
+  if (err) throw err;
+
+  console.log('connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema, table_name FROM information_schema.tables;')
+    .on('row', (row) => {
+      console.log(JSON.stringify(row));
+    });
+});
 
 // var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
